9987392a00784571aa606e2f0e9977d413ac46b6
Dennis <vuhuy999999@gmail.com>
2024-10-20 00:26:48 +0800

    the second time

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] =  'SECRET_KEY'
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/')
 def index():
@@ -18,6 +27,31 @@ def index():
 
 def user(name):
     return render_template('user.html', name=name)
+
+
+# Create an error page
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('form.html', 
+                           form=form, 
+                           name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
