c3b658e5e81cf1637a058231092c29825ee6887b
Dennis <vuhuy999999@gmail.com>
2024-10-21 02:01:33 +0800

    user add

--- hello.py
+++ hello.py
@@ -2,14 +2,59 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# add db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
 app.config['SECRET_KEY'] =  'SECRET_KEY'
+# initialize the database
+db = SQLAlchemy(app)
+
+# Create a db class
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80),nullable=False)
+    email = db.Column(db.String(80),nullable=False, unique=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+ 
+    def __repr__(self):
+        return f"<User {self.name}>"
+
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+@app.route('/form/add', methods=['GET', 'POST'])
+def user_add():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            flash('User already exists, please change your email')
+            
+        else:
+            user = User(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash('User added successfully')
+    users = User.query.order_by(User.date.asc()).all()
+    return render_template('user_add.html',
+                           form=form,
+                           name=name,
+                           users=users)
 
 
 @app.route('/')
